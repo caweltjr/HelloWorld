91b251f49fc1bdb226562612a0ff0b63ecabdc30
John Cawelti <caweltjr@hotmail.com>
2025-02-26 12:14:36 -0800

    Add logging to HelloController

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -2,12 +2,16 @@ package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
+    private static final Logger LOGGER = Logger.getLogger(HelloController.class.getName());
+
     @GetMapping(value = {"/", ""})
     public String hello() {
-        System.out.println("Hello endpoint hit!");
+        LOGGER.info("Hello endpoint hit!");
+        System.out.println("Hello endpoint hit via System.out!");
         return "Hello, World!";
     }
 }
